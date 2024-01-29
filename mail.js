7fae08b7f1f5406ce943ae5d14adf779192a4d46
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2024-01-29 17:19:00 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -21,45 +21,10 @@ setInterval(() => {
 }, 30000);
 
 
-// function sendMail() {
-//     var paras = {
-//         name: document.getElementById("name").value,
-//         email: document.getElementById("email").value,
-//         messege: document.getElementById("Messege").value
-
-//     };
-//     console.log(paras);
-//     const serviceid = "service_f61kjpb";//service_f61kjpb
-//     const templateid = "template_xejkctk";//template_xejkctk
-
-
-//     emailjs.send(serviceid, templateid, paras)
-//         .then((res) => {
-//             document.getElementById("name").value = "";
-//             document.getElementById("email").value = "";
-//             document.getElementById("Messege").value = "";
-//             alert("messege send SucessFully")
-//             console.log(res);
-
-//         })
-//         .catch((err => console.log(err)))
-}
-
 let by = () => {
     alert("Welcome To My Page   ")
 }
 
-
-    // window.addEventListener("scroll", () => {
-    //     if (window.scrollY > 100) {
-    //         icon.classList.add("active")
-
-    //     }
-    //     else {
-    //         icon.classList.remove("active")
-    //     }
-
-    // })
 function along() {
      window.scrollTo(0,0)
 }
