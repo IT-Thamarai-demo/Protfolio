d63d4a4cae921b90d9644119d1a2b29024c7701c
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2024-01-29 17:26:55 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,29 +1,29 @@
 
 
-let body = document.querySelector("body");
-let btn1 = document.querySelector("#change");
-let icon = document.querySelector(".scroll")
-let text=document.querySelector("#c");
-console.log(btn1);
+// let body = document.querySelector("body");
+// let btn1 = document.querySelector("#change");
+// let icon = document.querySelector(".scroll")
+// let text=document.querySelector("#c");
+// console.log(btn1);
 
-btn1.addEventListener("click", () => {
-    body.classList.toggle("mall")
+// btn1.addEventListener("click", () => {
+//     body.classList.toggle("mall")
 
-})
-setInterval(() => {
-    text.textContent = "Free Lancer";
-}, 10000);
-setInterval(() => {
-    text.textContent = "Web Desinnger";
-}, 20000);
-setInterval(() => {
-   text.textContent = "Content Createar";
-}, 30000);
+// })
+// setInterval(() => {
+//     text.textContent = "Free Lancer";
+// }, 10000);
+// setInterval(() => {
+//     text.textContent = "Web Desinnger";
+// }, 20000);
+// setInterval(() => {
+//    text.textContent = "Content Createar";
+// }, 30000);
 
 
-let by = () => {
-    alert("Welcome To My Page   ")
-}
+// let by = () => {
+//     alert("Welcome To My Page   ")
+// }
 
 function along() {
      window.scrollTo(0,0)
