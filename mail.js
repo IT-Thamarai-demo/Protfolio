f65adb96b784fccafa0bb67e3d8ce12d4ec52d86
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2024-01-29 12:33:06 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,51 +1,57 @@
-let name1=document.querySelector("#c");
 
-let body=document.querySelector("body");
-let btn1=document.querySelector("#change");
+
+let body = document.querySelector("body");
+let btn1 = document.querySelector("#change");
+let icon = document.querySelector(".scroll")
+let text=document.querySelector("#c");
 console.log(btn1);
-      
-       btn1.addEventListener("click",()=>
 
-       {
-         body.classList.toggle("mall")
-        
-       })
-       setInterval(() => {
-            name1.textContent="Free Lancer";
-       }, 10000);
-       setInterval(() => {
-           name1.textContent="Web Desinger";
-       }, 20000);
-       setInterval(() => {
-           name1.textContent="Content Creator";
-       }, 30000);
-        
+btn1.addEventListener("click", () => {
+    body.classList.toggle("mall")
+
+})
+setInterval(() => {
+    text.textContent = "Free Lancer";
+}, 10000);
+setInterval(() => {
+    text.textContent = "Web Desinnger";
+}, 20000);
+setInterval(() => {
+   text.textContent = "Content Createar";
+}, 30000);
+
 
 function sendMail() {
-    var paras={
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        messege:document.getElementById("messege").value
+    var paras = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        messege: document.getElementById("Messege").value
 
     };
     console.log(paras);
-        const serviceid="service_f61kjpb";//service_f61kjpb
-    const templateid="template_xejkctk";//template_xejkctk
-    
+    const serviceid = "service_f61kjpb";//service_f61kjpb
+    const templateid = "template_xejkctk";//template_xejkctk
 
-emailjs.send(serviceid,templateid,paras)
-.then((res)=>{
-        document.getElementById("name").value="";
-        document.getElementById("email").value="";
-        document.getElementById("messege").value="";
-        console.log(res);
-        alert("messege send SucessFully")
-})
-.catch((err => console.log(err) ))
+
+    emailjs.send(serviceid, templateid, paras)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("Messege").value = "";
+            alert("messege send SucessFully")
+            console.log(res);
+
+        })
+        .catch((err => console.log(err)))
+}
+console.log("called");
+let by = () => {
+    alert("Welcome To My Page   ")
 }
 
+
     // window.addEventListener("scroll", () => {
-    //     if (window.scrollY > 400) {
+    //     if (window.scrollY > 100) {
     //         icon.classList.add("active")
 
     //     }
@@ -53,12 +59,8 @@ emailjs.send(serviceid,templateid,paras)
     //         icon.classList.remove("active")
     //     }
 
-    })
+    // })
 function along() {
      window.scrollTo(0,0)
 }
-let by=()=>
-      {
-        alert("𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝗧𝗼 𝗠𝘆 𝗣𝗿𝗼𝘁𝗳𝗼𝗹𝗶𝗼")
 
-      }
