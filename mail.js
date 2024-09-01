88cf319f48495f85cd16d358397c76d646dc0e56
Thamaraikannan <120456321+IT-Thamarai-demo@users.noreply.github.com>
2024-09-01 11:39:02 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -34,7 +34,7 @@ if (widthElement) widthElement.textContent = width;
 if (heightElement) heightElement.textContent = height;
 
 // Logging navigation tag
-let tag = document.querySelector("#dan");
+let tag = document.querySelector(".dialog");
 console.log(tag);
 
 // Contact form submission using Formspree
